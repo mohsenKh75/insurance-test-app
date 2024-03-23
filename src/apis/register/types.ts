d0106ad95c2a036ce4cf3bd7eaf8ef6ef0448359
Mohsen Khademhosseini <mohsenpm77@gmail.com>
2024-03-23 18:46:20 +0330

    feat: add register ep - postReq func

---
+++ src/apis/register/types.ts
@@ -0,0 +1,11 @@
+interface RegisterType {
+  firstName: string;
+  lastName: string;
+  password: string;
+}
+
+interface RegisterReturnTypes {
+  first_name: string;
+  family_name: string;
+  password: string;
+}
