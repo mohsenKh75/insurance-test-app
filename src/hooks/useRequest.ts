8f73eba822b1b4b100c3a9fffb86a7717399727e
Mohsen Khademhosseini <mohsenpm77@gmail.com>
2024-03-23 18:09:58 +0330

    feat: add func to handle req - hooks for get-post data

---
+++ src/hooks/useRequest.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+export function useRequest(request?: () => Promise<any>) {
+  const [data, setData] = useState<any>([]);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState<unknown>(null); // State to track errors
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await request?.();
+        setData(response);
+        setError(null);
+      } catch (err) {
+        console.error("error on fetching data:", err);
+        setError(err);
+      } finally {
+        setIsPending(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { isPending, data, error };
+}
